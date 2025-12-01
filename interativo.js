9d65aa84189051534d38783e7a359e6bcc6eafd8
Mateusggff <mateusgabrielferreirafagundes@gmail.com>
2025-11-30 23:46:20 -0300

    Add files via upload

--- interativo.js
+++ interativo.js
@@ -1,5 +1,5 @@
 const personagem = document.querySelector(".pessoa");
-const barreiras = document.querySelector(".barreiras");
+const inimigoEl = document.querySelector(".inimigo");
 
 let tempo = 0;
 let tempoInterval;
@@ -8,11 +8,11 @@ let jogoAtivo = false;
 let imagemFundo = document.querySelector('.principal');
 
  let nomeDaImagem = [
-    'https://png.pngtree.com/thumb_back/fw800/background/20250731/pngtree-volcano-erupting-background-image_17673419.webp',
+    'https://epicorpg.com.br/wp-content/uploads/2019/03/caverna-complexo.gif',
+    'https://i.pinimg.com/originals/e8/99/f8/e899f8cc9ee082c8743abdfe7d66fa5b.gif',
  ];
-
- function atualizarImagem(){
-  const caminhoDaImagem = nomeDaImagem[0];
+ function atualizarImagem(a){
+  const caminhoDaImagem = nomeDaImagem[a];
   
   imagemFundo.style.backgroundImage = `url(${caminhoDaImagem})`;
 }
@@ -25,16 +25,29 @@ function iniciarTimer() {
         tempo++;
         document.getElementById("timer").textContent = `Tempo: ${tempo}s`;
 
-        if (tempo === 5) barreiras.style.animationDuration = "3.5s";
-        if (tempo === 10) barreiras.style.animationDuration = "3.8s";
-        if (tempo === 15) barreiras.style.animationDuration = "2.0s";
-        if (tempo === 30){
-            barreiras.style.animationDuration = "1.0s";
+        if (tempo === 5) document.documentElement.style.setProperty("--velocidade", "3.5s");
+        if (tempo === 10) document.documentElement.style.setProperty("--velocidade", "3s");
+        if (tempo === 15) document.documentElement.style.setProperty("--velocidade", "2.5s");
+        if (tempo === 20){
+            document.documentElement.style.setProperty("--velocidade", "2s");
 
-            atualizarImagem();
+            atualizarImagem(0);
+            inimigoEl.src = 'https://art.ngfiles.com/comments/1055000/iu_1055299_8284187.gif';
+            inimigoEl.style.width = "400px";
+            inimigoEl.style.height = "300px";
+            inimigoEl.style.bottom = "-100px";
+            personagem.style.bottom = "-60px";
         } 
-        if (tempo === 40) barreiras.style.animationDuration = "0.9s";
-        if (tempo === 60) barreiras.style.animationDuration = "0.8s";
+        if (tempo === 30){ document.documentElement.style.setProperty("--velocidade", "1.5s")
+            atualizarImagem(1);
+
+            inimigoEl.src = 'https://i.pinimg.com/originals/66/03/ab/6603ab4db145ecb11873519ccc3d3055.gif';
+
+            inimigoEl.style.width = "400px";
+            inimigoEl.style.height = "300px";
+            inimigoEl.style.bottom = "-50px";
+        }
+        if (tempo === 60) document.documentElement.style.setProperty("--velocidade", "1s");
     }, 1000);
 }
 
@@ -42,22 +55,14 @@ function pararTimer() {
     clearInterval(tempoInterval);
 }
 
-
-let index = 0;
-setInterval(() => {
-    barreiras.src = imagens[index];
-    index = (index + 1) % imagens.length;
-}, 1000);
-
 const loopColisao = setInterval(() => {
     if (!jogoAtivo) return;
 
-    const barreirasPosition = barreiras.offsetLeft;
+    const inimigoElPosition = inimigoEl.offsetLeft;
     const pessoaPosition = +window.getComputedStyle(personagem).bottom.replace('px', '');
 
-    if (barreirasPosition <= 70 && barreirasPosition > 0 && pessoaPosition < 80) {
-        barreiras.style.animation = 'none';
-        barreiras.style.left = `${barreirasPosition}px`;
+    if (inimigoElPosition <= 70 && inimigoElPosition > 0 && pessoaPosition < 80) {
+        inimigoEl.style.animation = 'none';
         personagem.style.animation = 'none';
         personagem.src = 'https://png.pngtree.com/png-clipart/20230914/original/pngtree-tombstone-clipart-cartoon-grave-with-a-skull-and-grass-vector-png-image_11091955.png';
         personagem.style.width = '8%';
@@ -74,7 +79,7 @@ document.addEventListener("keydown", (e) => {
     if (e.code === "Space") {
         if (!jogoAtivo) {
             jogoAtivo = true;
-            barreiras.style.animation = 'barreiras 4s infinite linear';
+            inimigoEl.style.animation = 'inimigo var(--velocidade) linear infinite';
             iniciarTimer();
         }
 
@@ -87,3 +92,27 @@ document.addEventListener("keydown", (e) => {
 });
 
 //----------------------------------------------------
+
+
+let engrenagem = document.querySelector('#alterna-menu');
+let bodyEl = document.querySelector('body');
+
+engrenagem.addEventListener('click', function(){
+    bodyEl.classList.toggle('menu-visivel');
+});
+
+
+/*-----------------MUSICA------------------------*/
+let somEl = document.querySelector('#botaoAudio');
+let musicaEl = document.querySelector('#musica');
+
+somEl.addEventListener('click', function(){
+    somEl.classList.toggle('selecionado');
+
+    if(musicaEl.paused){
+        musicaEl.play();
+    }else{
+        musicaEl.pause();
+    }
+});
+ 
